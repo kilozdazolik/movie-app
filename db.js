ee50e28702bb283165f0a16d271ead3cd41c0632
kilozdazolik <zotyibogyo@gmail.com>
2022-10-15 15:03:04 +0200

    javascript implementation -> db

---
+++ db.js
@@ -0,0 +1,29 @@
+const movieList = {
+  currency: "HUF",
+  movies: [
+    {
+      title: "Top Gun Maverick",
+      price: 15,
+    },
+    {
+      title: "John Wick",
+      price: 10,
+    },
+    {
+      title: "American Psycho",
+      price: 12,
+    },
+    {
+      title: "Jarhead",
+      price: 8,
+    },
+  ],
+};
+
+const seatsDeatils = {
+  rows: 6,
+  columns: 8,
+  occupied: [11, 12, 28, 29, 35, 36, 37, 38, 44],
+};
+
+export { movieList, seatsDeatils };
